ecc5b5903daf3b83479f372092e4b97196c7f582
Thara Gopinath <thara.gopinath@linaro.org>
2021-09-22 09:00:01 -0400

    crypto: qce: Add support for sm8150 crypto engine.
    Signed-off-by: Thara Gopinath <thara.gopinath@linaro.org>

--- drivers/crypto/qce/core.c
+++ drivers/crypto/qce/core.c
@@ -300,6 +300,7 @@ static int qce_crypto_remove(struct platform_device *pdev)
 static const struct of_device_id qce_crypto_of_match[] = {
 	{ .compatible = "qcom,crypto-v5.1", },
 	{ .compatible = "qcom,crypto-v5.4", },
+	{ .compatible = "qcom,crypto-sm8150", },
 	{}
 };
 MODULE_DEVICE_TABLE(of, qce_crypto_of_match);
